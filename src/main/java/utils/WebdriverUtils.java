142dad8e8791a7df2622daee96357bd47ccd3db7
Stefan Peshevski <Stefan.Peshevski@endava.com>
2024-10-31 17:24:43 +0100

    test

---
+++ src/main/java/utils/WebdriverUtils.java
@@ -0,0 +1,54 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+
+import java.time.Duration;
+
+public class WebdriverUtils {
+
+    public static WebDriver setUpDriver() {
+        String browser = "chrome";
+        String driverFolder = "C:\\Selenium";
+        String driverPathChrome = driverFolder + "\\chromedriver.exe";
+        String driverPathFirefox = driverFolder + "\\geckodriver.exe";
+        String driverPathEdge = driverFolder + "\\msedgedriver.exe";
+
+        WebDriver driver = null;
+
+        switch (browser) {
+            case "chrome": {
+                System.setProperty("webdriver.chrome.driver", driverPathChrome);
+                driver = new ChromeDriver();
+                break;
+            }
+            case "firefox": {
+                System.setProperty("webdriver.gecko.driver", driverPathFirefox);
+                driver = new FirefoxDriver();
+                break;
+            }
+            case "edge": {
+                System.setProperty("webdriver.edge.driver", driverPathEdge);
+                driver = new EdgeDriver();
+                break;
+            }
+            default: {
+                Assert.fail("Can't create driver! Browser: " + browser + " is not supported!");
+            }
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2));
+
+        return driver;
+    }
+
+    public static void quitDriver(WebDriver driver) {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -4,76 +4,59 @@ import data.TestGroups;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import utils.WebdriverUtils;
 
 import java.time.Duration;
 
 @Test(groups = {TestGroups.REGRESSION, TestGroups.LOGIN})
 public class LoginTests {
-    private static final String PATH_DRIVER_CHROME = "C:\\Selenium\\chromedriver.exe";
     private static final String BASE_URL = "https://www.saucedemo.com/";
-    private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", PATH_DRIVER_CHROME);
-        WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.manage().window().maximize();
-        chromeDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1));
-        chromeDriver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(2)); // waits for DOM structure of the page to be loaded
-        driver.set(chromeDriver);
-    }
-
-    public WebDriver getDriver() {
-        return driver.get();
-    }
 
     @Test(groups = {TestGroups.SANITY})
     public void testSuccessfulLogin() {
-        getDriver().get(BASE_URL);
+        WebDriver driver = WebdriverUtils.setUpDriver();
+        try {
+            driver.get(BASE_URL);
+            WebElement inputName = driver.findElement(By.id("user-name"));
+            inputName.sendKeys("standard_user");
 
-        WebElement inputName = getDriver().findElement(By.id("user-name"));
-        inputName.sendKeys("standard_user");
+            WebElement inputPassword = driver.findElement(By.id("password"));
+            inputPassword.sendKeys("secret_sauce");
 
-        WebElement inputPassword = getDriver().findElement(By.id("password"));
-        inputPassword.sendKeys("secret_sauce");
+            WebElement loginButton = driver.findElement(By.id("login-button"));
+            loginButton.click();
 
-        WebElement loginButton = getDriver().findElement(By.id("login-button"));
-        loginButton.click();
-
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(3)); // explicit wait
-        wait.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/inventory.html"));
-        WebElement pageTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-test='title']")));
-        Assert.assertEquals(pageTitle.getText(), "Products", "Incorrect page title!");
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(3)); // explicit wait
+            wait.until(ExpectedConditions.urlToBe("https://www.saucedemo.com/inventory.html"));
+            WebElement pageTitle = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("[data-test='title']")));
+            Assert.assertEquals(pageTitle.getText(), "Products", "Incorrect page title!");
+        } finally {
+            WebdriverUtils.quitDriver(driver);
+        }
     }
 
     @Test
     public void testUnsuccessfulLoginWrongPassword() {
-        getDriver().get(BASE_URL);
+        WebDriver driver = WebdriverUtils.setUpDriver();
+        try {
+            driver.get(BASE_URL);
+            WebElement inputName = driver.findElement(By.id("user-name"));
+            inputName.sendKeys("standard_user");
 
-        WebElement inputName = getDriver().findElement(By.id("user-name"));
-        inputName.sendKeys("standard_user");
+            WebElement inputPassword = driver.findElement(By.id("password"));
+            inputPassword.sendKeys("wrong_password");
 
-        WebElement inputPassword = getDriver().findElement(By.id("password"));
-        inputPassword.sendKeys("wrong_password");
+            WebElement loginButton = driver.findElement(By.id("login-button"));
+            loginButton.click();
 
-        WebElement loginButton = getDriver().findElement(By.id("login-button"));
-        loginButton.click();
+            Assert.assertEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/", "Wrong URL");
 
-        Assert.assertEquals(getDriver().getCurrentUrl(), "https://www.saucedemo.com/", "Wrong URL");
-    }
-
-    @AfterMethod
-    public void tearDown() {
-        if (getDriver() != null) {
-            getDriver().quit();
-            driver.remove();  // Clear the ThreadLocal instance
+        } finally {
+            WebdriverUtils.quitDriver(driver);
         }
     }
 }
