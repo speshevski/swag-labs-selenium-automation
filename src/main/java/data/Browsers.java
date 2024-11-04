df7b1290f1264f1ffcbccdfbea7f4e6fb6033c37
Stefan Peshevski <Stefan.Peshevski@endava.com>
2024-11-04 13:53:09 +0100

    tst

--- src/main/java/data/Browsers.java
+++ src/main/java/data/Browsers.java
@@ -1,6 +1,6 @@
 package data;
 
-public class Browsers {
+public final class Browsers {
     public static final String CHROME = "chrome";
     public static final String FIREFOX = "firefox";
     public static final String EDGE = "edge";
